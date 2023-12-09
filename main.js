20590aa5b976b83ef6de05fb1cb9dbace344b09e
d4c2b0 <129341151+d4c2b0@users.noreply.github.com>
2023-12-10 00:35:03 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const { app, BrowserWindow } = require('electron');
+const { OpenAI } = require("openai");
+
 require('dotenv').config()
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+async function chatTest() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(chatCompletion.choices[0].message.content);
+}
+
+chatTest();
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
