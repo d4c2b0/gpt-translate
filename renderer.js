b7536eb788afa3a8504f72f449e544ae663d1ca7
d4c2b0 <129341151+d4c2b0@users.noreply.github.com>
2023-12-09 17:58:37 -0800

    Create translation UI structure (#3)

--- renderer.js
+++ renderer.js
@@ -1,1 +1,14 @@
 console.log('Hello, world.');
+
+const translateForm = document.getElementById('translate-form')
+const translateInput = document.getElementById('translate-input')
+const translateOutput = document.getElementById('translate-output')
+
+translateForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const userInput = translateInput.value.trim();
+    if (!userInput) return;
+
+    translateOutput.textContent = userInput;
+})
